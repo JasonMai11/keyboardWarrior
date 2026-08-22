711e3de2b2d5a369c3246cb2ba2513351c2c5125
Jason Mai <86985751+JasonMai11@users.noreply.github.com>
2026-08-22 13:57:45 -0400

    Initial Commit

---
+++ KeyboardWarrior/KeyboardWarriorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardWarriorApp.swift
+//  KeyboardWarrior
+//
+//  Created by Jason Mai on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardWarriorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyboardWarriorTests/KeyboardWarriorTests.swift
@@ -0,0 +1,19 @@
+//
+//  KeyboardWarriorTests.swift
+//  KeyboardWarriorTests
+//
+//  Created by Jason Mai on 8/22/26.
+//
+
+import Testing
+@testable import KeyboardWarrior
+
+struct KeyboardWarriorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
